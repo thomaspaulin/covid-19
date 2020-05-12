03dcdcca77a28b0a4c43442d1a7bd287146cec20
Thomas <7918983+thomaspaulin@users.noreply.github.com>
2020-05-13 00:33:56 +0200

    Convert to functional component

--- src/component/information-drawer/drawer-footer/DrawerFooter.js
+++ src/component/information-drawer/drawer-footer/DrawerFooter.js
@@ -1,9 +1,9 @@
-class DrawerFooter extends React.Component {
-    render() {
-        return (
-            <div className="drawer-footer">
-                <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public">Protect yourself</a>
-            </div>
-        );
-    }
+import React from "react";
+
+export default function DrawerFooter() {
+    return (
+        <div className="drawer-footer">
+            <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public">Protect yourself</a>
+        </div>
+    );
 }
